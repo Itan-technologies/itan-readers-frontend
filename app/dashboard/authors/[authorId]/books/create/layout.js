967ff7d0311b862919ea9161257894945af765d0
Ol-create <oluola96@gmail.com>
2025-03-06 07:47:04 +0100

    add book pricing skeleton

--- app/dashboard/authors/[authorId]/books/create/layout.js
+++ app/dashboard/authors/[authorId]/books/create/layout.js
@@ -15,14 +15,14 @@ const layout = ({ children }) => {
   />
 </div>
 <div className="ml-2 flex justify-between items-center bg-gray-200 h-12 w-48 pr-2 rounded-md">
-  <p className="flex-1 text-center">Book Details</p>
+  <p className="flex-1 text-center">Book Content</p>
   <img
     src="/icons/check-mark.png"
     className="w-3 h-3"
   />
 </div>
 <div className="ml-2 flex justify-between items-center bg-gray-200 h-12 w-48 pr-2 rounded-md">
-  <p className="flex-1 text-center">Book Details</p>
+  <p className="flex-1 text-center">Book Pricing</p>
   <img
     src="/icons/check-mark.png"
     className="w-3 h-3"
