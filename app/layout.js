5db1d5d7d7d9b2f57f7a4d58ec9775800bd44db5
Ol-create <oluola96@gmail.com>
2025-03-11 13:15:43 +0100

    Wrap layout.js with FormProvider

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { FormProvider } from "../context/FormContext";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <FormProvider>{children}</FormProvider>
       </body>
     </html>
   );
