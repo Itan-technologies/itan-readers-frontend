8ccde6ed2da614dd4359aea61812d94c9d8cff6c
Ol-create <oluola96@gmail.com>
2025-03-26 22:02:51 +0100

    add html skeleton to publish page

---
+++ app/(authorLandingPage)/publish/page.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const Publish = () => {
+  return (
+    <div>
+        <section>
+        <div>
+          <h1>Publish With Itan Global Publishing (IGP)</h1>
+          <p>
+            With Itan Global Publishing (IGP), publish your books and
+             reach millions of readers around the world
+          </p>
+          <div>
+            <button>Sign In</button>
+            <button>Join Itan</button>
+          </div>
+        </div>
+        <img src="" alt="" />
+      </section>
+
+      <section>
+        <h2>Start your publishing journey with two easy steps</h2>
+        <h3>Step 1: Get your manuscript and cover ready</h3>
+        <p>Ensure your manuscript is properly formatted, including the 
+            cover page, front section, title page and author biography.</p>
+            <p>We recommend hiring a desktop publisher to format your book 
+                for publishing. Use Canva to crate a stunning and eye-catching cover, 
+                or work ith a graphic artist for a professionally designed cover.</p>
+      </section>
+
+      <section>
+        <h3>Step 2: Publish in digital formats</h3>
+        <p>You can publish your book as an ebook, an audiobook, or both, 
+            and we’ll make the available in Itan Global Stores.</p>
+        <div>
+            <ul>
+            <li>Input your book details, including the title and author, in the specified fields.</li>
+            <li>Choose your book categories.</li>
+            <li>Enter keywords and tags that best describes your book.</li>
+            <li>Submit your prepared manuscript and cover and review</li>
+            <li>Select your list prices for each book format.</li>
+            <li>Hit the “Publish” button.</li>
+        </ul>
+        <img src="" alt="" />
+        </div>
+      </section>
+
+      <section>
+        <img src="" alt="" />
+        <div>
+            <h2>Enhance Your Book’s Exposure - Start Promoting</h2>
+            <p>Congratulations on publishing your book! Next step: 
+                promote, promote, promote! Here are some suggestions:</p>
+                <ul>
+                    <li>Leverage Social Media by talking about your book and sharing the links.</li>
+                    <li>Utilize paid ad where necessary.</li>
+                    <li>Submit to Book review sites. Itan Book review service is coming soon.</li>
+                    <li>Use SEO Strategies, keywords and tags; Optimize your book’s description, title, and 
+                        keywords for search engines to improve visibilty and discoverability.</li>
+                </ul>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Publish;
