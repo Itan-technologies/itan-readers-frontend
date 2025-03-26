7a4684a8b5367ae117604e827ecc7419f66e4911
Ol-create <oluola96@gmail.com>
2025-03-26 22:25:54 +0100

    add html skeleton to monrtize page

---
+++ app/(authorLandingPage)/monetize/page.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Monetize = () => {
+  return (
+    <div>
+      <section>
+        <div>
+          <h1>Monetize With Itan Global Publishing (IGP)</h1>
+          <p>
+            Itan Global Publishing provides free publishing and multiple royalty
+            streams.
+          </p>
+          <div>
+            <button>Sign In</button>
+            <button>Join Itan</button>
+          </div>
+        </div>
+        <img src="" alt="" />
+      </section>
+
+      <section>
+        <h2>Maximize your Earning Through Multiple Revenue Sources</h2>
+        <ul>
+          <li>Earn up to 60% royalties on your ebooks</li>
+          <li>Earn up to 65% royalties on your audio books</li>
+          <li>Get paid for every page read with Itan Unbound</li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default Monetize
