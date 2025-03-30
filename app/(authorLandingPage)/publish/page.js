b7aa2ec999668d7c3c58da25587c2beebf23be81
Ol-create <oluola96@gmail.com>
2025-03-30 16:03:03 +0100

    add style to publish page

--- app/(authorLandingPage)/publish/page.js
+++ app/(authorLandingPage)/publish/page.js
@@ -39,31 +39,34 @@ const Publish = () => {
         className="z-30 relative bg-cover bg-center bg-no-repeat h-[450px] flex flex-col items-center justify-center text-center px-3 "
         style={{ backgroundImage: "url('/images/bg-img-publish.png')" }}
       >
-        <h2 className="text-3xl font-bold">
-          Start your publishing journey with two easy steps
+        <h2 className="text-2xl font-bold mt-5">
+          Start your <span className="text-[#EF5353]">publishing</span> journey
+          with two easy steps
         </h2>
-        <h3 className="text-xl font-semibold text-white mt-4">
-          Step 1: Get your manuscript and cover ready
-        </h3>
-        <p className="text-white mt-2 max-w-2xl">
+        <ul className="list-disc list-inside space-y-2 text-xl font-semibold text-left">
+          <li className="mt-4">Step 1: Get your manuscript and cover ready</li>
+        </ul>
+        <p className="my-3 max-w-2xl ml-4 text-left text-xs">
           Ensure your manuscript is properly formatted, including the cover
           page, front section, title page, and author biography.
         </p>
-        <p className="text-white mt-2 max-w-2xl">
+        <p className="mt-2 max-w-2xl text-left text-xs">
           We recommend hiring a desktop publisher to format your book for
           publishing. Use Canva to create a stunning and eye-catching cover, or
           work with a graphic artist for a professionally designed cover.
         </p>
       </section>
 
-      <section>
-        <h3>Step 2: Publish in digital formats</h3>
-        <p>
+      <section className="mx-3 mt-3">
+        <h3 className="text-2xl font-semibold">
+          Step 2: Publish in digital formats
+        </h3>
+        <p className="my-2 text-left">
           You can publish your book as an ebook, an audiobook, or both, and
           we’ll make the available in Itan Global Stores.
         </p>
         <div>
-          <ul>
+          <ul className="list-disc list-inside ml-1 text-xl space-y-2 font-semibold text-left">
             <li>
               Input your book details, including the title and author, in the
               specified fields.
@@ -74,19 +77,26 @@ const Publish = () => {
             <li>Select your list prices for each book format.</li>
             <li>Hit the “Publish” button.</li>
           </ul>
-          <img src="/images/digital-formats.png" alt="digital formats" />
+          <img
+            src="/images/digital-formats.png"
+            alt="digital formats"
+            className="hidden"
+          />
         </div>
       </section>
 
-      <section>
-        <img src="/images/exposure.png" alt="exposure" />
+      <section className="mx-3 mt-10">
+        <img src="/images/exposure.png" alt="exposure" className="hidden" />
         <div>
-          <h2>Enhance Your Book’s Exposure - Start Promoting</h2>
-          <p>
+          <h2 className="text-2xl font-semibold">
+            Enhance Your Book’s Exposure -{" "}
+            <span className="text-[#EF5353]">Start Promoting</span>
+          </h2>
+          <p className="text-left font-semibold my-2">
             Congratulations on publishing your book! Next step: promote,
             promote, promote! Here are some suggestions:
           </p>
-          <ul>
+          <ul className='list-disc list-inside ml-1 space-y-2 font-semibold text-left'>
             <li>
               Leverage Social Media by talking about your book and sharing the
               links.
