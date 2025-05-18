6d73ae2425c885ebc4392681674d7fdfdf806c19
Ol-create <oluola96@gmail.com>
2025-05-18 19:52:13 +0100

    add email confirmation message

---
+++ app/auth/confirm-email/page.js
@@ -0,0 +1,17 @@
+import { Suspense } from "react";
+import ConfirmEmailContent from "./ConfirmEmailContent";
+
+export default function ConfirmPage() {
+  return (
+    // <Suspense
+    //   fallback={
+    //     <div className="min-h-screen flex justify-center items-center">
+    //       <p className="text-lg">Processing confirmation...</p>
+    //     </div>
+    //   }
+    // >
+    //   <ConfirmEmailContent />
+    // </Suspense>
+    <h2>Email verified successfully</h2>
+  );
+}
