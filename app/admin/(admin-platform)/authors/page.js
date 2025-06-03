b6c2f12fbed1a67f04f2b5ad8c96bf130dab7c64
Ol-create <oluola96@gmail.com>
2025-06-03 11:17:35 +0100

    add admin payment page

--- app/admin/(admin-platform)/authors/page.js
+++ app/admin/(admin-platform)/authors/page.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSlidersH, faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+import { faSlidersH, faEllipsish} from "@fortawesome/free-solid-svg-icons";
 import {
   Table,
   TableBody,
--- app/admin/(admin-platform)/layout.js
+++ app/admin/(admin-platform)/layout.js
@@ -171,7 +171,7 @@ export default function AuthorDashboardLayout({ children }) {
             </li>
             <li>
               <Link
-                href="#"
+                href="payment"
                 className="flex items-center p-2 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <FontAwesomeIcon
