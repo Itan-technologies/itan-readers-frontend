82705b0c4affd2b84c0b8527dac340c44d5f944c
Ol-create <oluola96@gmail.com>
2025-06-24 15:12:18 +0100

    resolved readers home page

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,27 @@
-export const metadata = {
-//   title: {
-//     default: "IGP"
-//   },
-  icons: {
-    icon: "/images/itan-favicon.png",
-  },
-};
+"use client";
 
+import { usePathname } from "next/navigation";
 
-import RootLayout from "./RootLayout";
+import "flowbite";
+import "./globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+import ReadersFooter from "@/app/reader/(components)/ReadersFooter";
 
 export default function Layout({ children }) {
+  const pathname = usePathname();
+
+  const signUpPage = pathname.endsWith("/reader/sign_up")
+  const signInPage = pathname.endsWith("/reader/sign_in")
+
+  const hideRegPage =  signUpPage || signInPage ? "hidden" : "";
   return (
-    <html lang="en">
-      <body className="bg-gray-100">
-        <RootLayout>{children}</RootLayout>
+    <html lang="eng">
+      <body>
+        <main className="bg-gray-100">
+          {children}
+          <ReadersFooter hiddenPage={hideRegPage} />
+        </main>
       </body>
     </html>
   );
