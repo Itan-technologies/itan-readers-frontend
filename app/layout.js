91ec6260eaad48b5f32aea87a1571ed6d1dccc8a
Oluyemi Paul <oluola96@gmail.com>
2025-08-17 05:33:37 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,19 +12,20 @@ import ReadersFooter from "@/app/reader/(components)/ReadersFooter";
 export default function Layout({ children }) {
   const pathname = usePathname();
 
-  const signUpPage = pathname.endsWith("/reader/sign_up");
-  const signInPage = pathname.endsWith("/reader/sign_in");
-  const termsPage = pathname.endsWith("/terms&conditions");
-  const privacyPolicy = pathname.endsWith("/privacy-policies");
+  // const signUpPage = pathname.endsWith("/reader/sign_up");
+  // const signInPage = pathname.endsWith("/reader/sign_in");
+  // const termsPage = pathname.endsWith("/terms&conditions");
+  // const privacyPolicy = pathname.endsWith("/privacy-policies");
+  const showFooter = pathname === "/" || pathname.endsWith("/bookstore");
 
-  const hideRegPage =
-    signUpPage || signInPage || termsPage || privacyPolicy ? "hidden" : "";
+  // const hideRegPage =
+  //   signUpPage || signInPage || termsPage || privacyPolicy ? "hidden" : "";
   return (
     <html lang="eng">
       <body>
         <main className="bg-gray-100">
           <AuthProvider>{children}</AuthProvider>
-          <ReadersFooter hiddenPage={hideRegPage} />
+          <ReadersFooter hiddenPage={showFooter} />
         </main>
       </body>
     </html>
